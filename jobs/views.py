1eaa2de76d03ad3af77805e403948efa0d1d8ead
Deepak Bhaskaran <deepak@duo.com>
2017-08-26 11:59:01 -0400

    First working version of simple jobs app

---
+++ jobs/views.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello world, you've reached the jobs index page")
+
+def viewjobs(request):
+    return HttpResponse("Hello world, you can view jobs")
+
+def addjobs(request):
+    return HttpResponse("Hello world, you can add jobs")
