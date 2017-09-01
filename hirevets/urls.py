80e6ac67ff875b00d0f14056e2dd796cecaa4926
Deepak Bhaskaran <deepak@duo.com>
2017-09-01 09:47:35 -0400

    Pointed / to the jobs app

--- hirevets/urls.py
+++ hirevets/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^jobs/', include('jobs.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('jobs.urls', namespace='jobs')),
 ]
