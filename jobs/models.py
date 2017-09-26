1761d60ecb970a2b7cd42989b85f2a047863f668
Deepak Bhaskaran <deepak@duo.com>
2017-09-26 01:00:34 -0400

    Added functionality behind /jobs/viewjobs

--- jobs/models.py
+++ jobs/models.py
@@ -4,3 +4,22 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    company_id = models.AutoField(primary_key=True)
+    company_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.company_name)
+
+class Job(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    location_city = models.CharField(max_length=100)
+    location_state = models.CharField(max_length=2)
+    job_summary = models.CharField(max_length=200)
+    experience = models.CharField(max_length=200)
+    benefits = models.CharField(max_length=200)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- jobs/views.py
+++ jobs/views.py
@@ -6,11 +6,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import Job, Company
+
 def index(request):
     return render(request, 'index.html', context={})
 
 def viewjobs(request):
-    return HttpResponse("Hello world, you can view jobs")
+    return render(request, 'jobs.html', {'jobs': Job.objects.all()})
 
 def addjobs(request):
     return HttpResponse("Hello world, you can add jobs")
